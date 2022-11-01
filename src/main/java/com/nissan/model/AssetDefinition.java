6e873057cb1e9db92595ee0054bc78ce8a63896e
fatimashafana <fatimashafana888@gmail.com>
2022-11-01 17:56:37 +0530

    2nd commit

--- src/main/java/com/nissan/model/AssetDefinition.java
+++ src/main/java/com/nissan/model/AssetDefinition.java
@@ -95,6 +95,12 @@ public class AssetDefinition {
 	public void setAdClass(String adClass) {
 		this.adClass = adClass;
 	}
+
+
+	public Object findAssetById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
 
--- src/main/java/com/nissan/repo/IAssetDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.nissan.repo;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-import com.nissan.model.AssetDefinition;
-
-public interface IAssetDefinition extends JpaRepositoryImplementation<AssetDefinition, Integer> {
-
-}
