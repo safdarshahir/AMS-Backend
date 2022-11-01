c6bc4f759a0ec44b580ade4e7298bc5589977d9a
safdarshahir <safdarshahir@gmail.com>
2022-11-01 10:04:57 +0530

    Added project files

---
+++ src/main/java/com/nissan/Ams2022Application.java
@@ -0,0 +1,13 @@
+package com.nissan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ams2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ams2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nissan/Ams2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nissan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ams2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
